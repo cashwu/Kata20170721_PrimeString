b4ccf46a44c7e77800c65b1eae6fc8288fd2431b
cash <cash@cashwu.com>
2017-07-22 09:15:34 +0800

    first test case : aa should not be prime string

--- Kata_PrimeString/PrimeStringTests.cs
+++ Kata_PrimeString/PrimeStringTests.cs
@@ -12,6 +12,18 @@ namespace Kata_PrimeString
             AssertShouldBePrimeString("a");
         }
 
+        [TestMethod]
+        public void aa_Should_Not_Be_PrimeString()
+        {
+            AssertShouldNotBePrimeString("aa");
+        }
+
+        private static void AssertShouldNotBePrimeString(string s)
+        {
+            var actual = new Kata().PrimeString(s);
+            Assert.IsFalse(actual);
+        }
+
         private static void AssertShouldBePrimeString(string s)
         {
             var actual = new Kata().PrimeString(s);
@@ -23,6 +35,17 @@ namespace Kata_PrimeString
     {
         public bool PrimeString(string s)
         {
+            if (s.Length == 1)
+            {
+                return true;
+            }
+
+            var primeString = s.Substring(0, 1);
+            if (s.Replace(primeString, "") == "")
+            {
+                return false;
+            }
+
             return true;
         }
     }
