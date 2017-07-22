a4e305f42ccdd6eb443f8f7481965ba109eee9ab
cash <cash@cashwu.com>
2017-07-22 08:30:12 +0800

    first test case : a should be prime string

---
+++ Kata_PrimeString/PrimeStringTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata_PrimeString
+{
+    [TestClass]
+    public class PrimeStringTests
+    {
+        [TestMethod]
+        public void a_Should_Be_PrimeString()
+        {
+            AssertShouldBePrimeString("a");
+        }
+
+        private static void AssertShouldBePrimeString(string s)
+        {
+            var actual = new Kata().PrimeString(s);
+            Assert.IsTrue(actual);
+        }
+    }
+
+    public class Kata
+    {
+        public bool PrimeString(string s)
+        {
+            return true;
+        }
+    }
+}
